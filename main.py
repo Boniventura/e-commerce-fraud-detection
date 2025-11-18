c1917bad24f1d837f89e23193e0a6e6d3c225f8f
kacperq98 <brejeckik3@gmail.com>
2025-11-18 22:21:18 +0100

    Changes related with OOP

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-import pandas as pd
-from pprint import pprint
-import kagglehub
-import yaml
-import os
-from ydata_profiling import ProfileReport
+import utils
+import logging
 
-import utils 
+from src.data_loader import DataLoader
+from src.exploratory_data_analysis import ExplaratoryDataAnalysis
+from src.feature_engineering import FeatureEngineering
 
-CONFIG_FILE = "config.yaml"
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
 
-def main():
-	if __name__ == "__main__":
-
-		df, path = utils.load_dataset()
-		# utils.first_view_into_data(df)
-
-
-		utils.feature_engineering(df)
-
-		utils.first_view_into_data(df)
-		utils.clean_dataset_path(path) 
-main()
+if __name__ == "__main__":
+    dataset = DataLoader().run_data_loader()
+    ExplaratoryDataAnalysis(dataset).view_into_data(generate_html_report=False)
+    dataset = FeatureEngineering(dataset).run_feature_engineering()
+    ExplaratoryDataAnalysis(dataset).view_into_data(generate_html_report=False)
+    #ModelTrainig.train()
+    #ModelTrainig.evaluate()
+    #ModelTrainig.optimize_hyperparameters()
+    #Inferences.show_inferences()
+    
+    #PoC -Structure of the project 
+    #Data Loader 
+    # |
+    # Exploratory Data Analysis before feature engineering
+    # |
+    # Feature Engineering
+    # |
+    # Exploratory Data Analysis after feature engineering (dont know how to create API - need consideration)
+    # |
+    # Model Training, Evaluation and Optimization
+    # |
+    # Inferences
+ 
