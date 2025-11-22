07c8409be07cf5af94da77ec5915e2028e73dfbb
kacperq98 <brejeckik3@gmail.com>
2025-11-22 12:51:15 +0100

    common file created

--- src/common.py
+++ src/common.py
@@ -1,2 +1,15 @@
-class Common():
-    pass
+import os 
+import matplotlib.pyplot as plt
+
+def save_plot(folder_path = None, file_name = None):
+    try: 
+        full_path = f'{folder_path}/{file_name}'
+
+        os.makedirs(folder_path, exist_ok=True) 
+        
+        plt.savefig(full_path, dpi=300, bbox_inches='tight')
+
+        print(f"Wykres zapisano w: {full_path}")
+
+    except Exception as e:
+        print(f"Nie udało się zapisać wykresu. Błąd: {e}")
