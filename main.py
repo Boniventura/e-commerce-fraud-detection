bed4db4e03d325c7ad4bc7cb1a0b11af01f7d48d
kacperq98 <brejeckik3@gmail.com>
2025-11-22 21:58:39 +0100

    Created Pipeline class in main file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import utils
 import logging
 
 from src.data_loader import DataLoader
 from src.exploratory_data_analysis import ExplaratoryDataAnalysis
 from src.feature_engineering import FeatureEngineering
+
+logger = logging.getLogger(__name__)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,15 +14,33 @@ logging.basicConfig(
     ]
 )
 
+class Pipeline():
+    def __init__(self):
+        pass
+
+    def run(self):
+        logger.info("Pipeline Initalized")
+        logger.info("Started data loading process...")
+        dataset = DataLoader().run_data_loader()
+        
+        logger.info("Started exploratory data analysis...")
+        ExplaratoryDataAnalysis(dataset).view_into_data(generate_html_report=False)
+        ExplaratoryDataAnalysis(dataset).get_plots()
+        
+        logger.info("Started feature engineering process...")
+        FeatureEngineering(dataset).run_feature_engineering()
+        
+        ExplaratoryDataAnalysis(dataset).get_plots()
+        #ModelTrainig.train()
+        #ModelTrainig.evaluate()
+        #ModelTrainig.optimize_hyperparameters()
+        #Inferences.show_inferences()
+        
+
 if __name__ == "__main__":
-    dataset = DataLoader().run_data_loader()
-    ExplaratoryDataAnalysis(dataset).view_into_data(generate_html_report=False)
-    dataset = FeatureEngineering(dataset).run_feature_engineering()
-    ExplaratoryDataAnalysis(dataset).view_into_data(generate_html_report=False)
-    #ModelTrainig.train()
-    #ModelTrainig.evaluate()
-    #ModelTrainig.optimize_hyperparameters()
-    #Inferences.show_inferences()
+    pipeline = Pipeline()
+    pipeline.run()
+
     
     #PoC -Structure of the project 
     #Data Loader 
