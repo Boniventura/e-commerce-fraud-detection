b450628efacffb1481d26b35e61255a96074310b
kacperq98 <brejeckik3@gmail.com>
2025-11-22 21:59:26 +0100

    Changed languafe to English in common.py

--- src/common.py
+++ src/common.py
@@ -1,15 +1,19 @@
 import os 
 import matplotlib.pyplot as plt
+import pandas as pd
+import logging
+logger = logging.getLogger(__name__)
 
 def save_plot(folder_path = None, file_name = None):
     try: 
         full_path = f'{folder_path}/{file_name}'
 
         os.makedirs(folder_path, exist_ok=True) 
-        
+
         plt.savefig(full_path, dpi=300, bbox_inches='tight')
 
-        print(f"Wykres zapisano w: {full_path}")
+        print(f"plot saved in: {full_path}")
 
     except Exception as e:
-        print(f"Nie udało się zapisać wykresu. Błąd: {e}")
+        print(f"Can not save the plot. Error: {e}")
+
