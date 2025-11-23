0840557acd7015df0c117debfac48459247a5a0c
kacperq98 <brejeckik3@gmail.com>
2025-11-23 13:18:33 +0100

    Refactor in main file

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ logger = logging.getLogger(__name__)
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format    ='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler()
+        logging.FileHandler('logs/pipeline.log') 
     ]
 )
 
@@ -24,13 +25,22 @@ class Pipeline():
         dataset = DataLoader().run_data_loader()
         
         logger.info("Started exploratory data analysis...")
-        ExplaratoryDataAnalysis(dataset).view_into_data(generate_html_report=False)
-        ExplaratoryDataAnalysis(dataset).get_plots()
-        
+        eda = ExplaratoryDataAnalysis(dataset)
+        eda.view_into_data()
+        eda.create_report(generate_html_report=True, title ="Dataset report before Feature Engineering")
         logger.info("Started feature engineering process...")
-        FeatureEngineering(dataset).run_feature_engineering()
         
-        ExplaratoryDataAnalysis(dataset).get_plots()
+        fe = FeatureEngineering(dataset)
+        fe.run_feature_engineering()
+
+        logger.info("Started exploratory data analysis after feature enginerring...")
+        eda = ExplaratoryDataAnalysis(dataset)
+        eda.get_plots(folder_name="AfterFeatureEngineering")
+        eda.create_report(generate_html_report=True, title ="Dataset report after Feature Engineering")
+
+        fe = FeatureEngineering(dataset)
+        
+
         #ModelTrainig.train()
         #ModelTrainig.evaluate()
         #ModelTrainig.optimize_hyperparameters()
